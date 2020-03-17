9fd8eeaf7f33a909567988c51f24efd2629b6ca8
Tamas Kadar <ktamas@ktamas.com>
2020-03-17 14:34:23 +0100

    Whatever

--- printer.py
+++ printer.py
@@ -2,7 +2,7 @@ import hardware
 import image_data
 import skimage.io
 import skimage as ski
-import instakit as ik
+# import instakit as ik
 
 class Paperang_Printer:
     def __init__(self):
@@ -12,9 +12,9 @@ class Paperang_Printer:
 
     def print_image_file(self, path):
         if self.printer_hardware.connected:
-            # self.printer_hardware.sendSelfTestToBt()
-            self.printer_hardware.sendImageToBt(image_data.binimage2bitstream(
-                image_data.im2binimage(ski.io.imread(path),conversion="threshold")))
+            self.printer_hardware.sendSelfTestToBt()
+            # self.printer_hardware.sendImageToBt(image_data.binimage2bitstream(
+            #     image_data.im2binimage(ski.io.imread(path),conversion="threshold")))
     
     def print_dithered_image(self, path):
         if self.printer_hardware.connected:
@@ -22,8 +22,9 @@ class Paperang_Printer:
 
 if __name__=="__main__":
     mmj=Paperang_Printer()
+    mmj.print_image_file("whatever")
     # mmj.print_dithered_image("/Users/ktamas/Downloads/frame.png")
-    mmj.print_dithered_image("/Users/ktamas/Pictures/hard-job-being-a-baby.jpeg")
-    mmj.print_dithered_image("/Users/ktamas/Desktop/-km49qIJ_400x400.png")
-    mmj.print_dithered_image("/Users/ktamas/Downloads/10827905_10152921795874452_6300515507948976079_o.jpg")
+    # mmj.print_dithered_image("/Users/ktamas/Pictures/hard-job-being-a-baby.jpeg")
+    # mmj.print_dithered_image("/Users/ktamas/Desktop/-km49qIJ_400x400.png")
+    # mmj.print_dithered_image("/Users/ktamas/Downloads/10827905_10152921795874452_6300515507948976079_o.jpg")
     # mmj.print_dithered_image("/Users/ktamas/Downloads/10827905_10152921795874452_6300515507948976079_o.jpg")

54c8072d75c137a286f378adfca350e96b56c959
Tamas Kadar <ktamas@ktamas.com>
2020-04-19 11:23:25 +0200

    config file

--- littleprinter.py
+++ littleprinter.py
@@ -2,6 +2,7 @@ import time
 import hardware
 import image_data
 from watchgod import watch
+import config
 
 class Paperang_Printer:
     def __init__(self):
@@ -13,7 +14,7 @@ class Paperang_Printer:
 
 if __name__ == '__main__':
     mmj=Paperang_Printer()
-    for changes in watch('/root/tmp'):
+    for changes in watch(config.tmpdir):
         file = changes.pop()[1] 
         print("Printing " + file)
         mmj.print_sirius_image(file)
